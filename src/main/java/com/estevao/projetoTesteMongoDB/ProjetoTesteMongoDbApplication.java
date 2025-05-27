96a7840c526460a2e8a0b7b7d114d13aaaf1bd1b
Estevao <estevoliveira1@gmail.com>
2025-05-26 21:50:40 -0300

    first commit

---
+++ src/main/java/com/estevao/projetoTesteMongoDB/ProjetoTesteMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.estevao.projetoTesteMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTesteMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTesteMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estevao/projetoTesteMongoDB/ProjetoTesteMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estevao.projetoTesteMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTesteMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
