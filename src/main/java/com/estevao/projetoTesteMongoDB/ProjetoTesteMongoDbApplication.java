552cb2434bdc52c59c84428cf02da8004e0482a7
Estevao <estevoliveira1@gmail.com>
2025-12-16 20:33:07 -0300

    vai tiooo

--- src/main/java/com/estevao/projetoTesteMongoDB/ProjetoTesteMongoDbApplication.java
+++ src/main/java/com/estevao/projetoTesteMongoDB/ProjetoTesteMongoDbApplication.java
@@ -1,13 +1,19 @@
 package com.estevao.projetoTesteMongoDB;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoTesteMongoDbApplication {
+public class ProjetoTesteMongoDbApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoTesteMongoDbApplication.class, args);
 	}
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Aplicação iniciada com sucesso!");
+    }
 }
